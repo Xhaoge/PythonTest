9850c6184ec7202cf6f689a21955fc055f4fdd45
nima <1026737316@qq.com>
2019-06-18 23:16:27 +0800

    0618

--- biquTest.py
+++ biquTest.py
@@ -9,7 +9,16 @@ import requests
 '''
 
 url = "http://www.biqukan.com/"
-url1 = "https://www.biqukan.com/1_1094/5978227.html"
+url1 = "https://www.biqukan.com/1_1094/5403177.html"
 
-req = requests.get(url1)
+# req = requests.get(url1)
+# print(req.text)
+
+
+'''
+爬取高清图片
+'''
+
+url3 = "https://unsplash.com/"
+req = requests.get(url=url3)
 print(req.text)

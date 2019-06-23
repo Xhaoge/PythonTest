a578ef80ca4219525a62e9934e415a8318400d4a
xxx <1026737316@qq.com>
2019-06-23 15:17:43 +0800

    add maoyan

---
+++ TAT/maoyanmovie.py
@@ -0,0 +1,59 @@
+#!/usr/lib/python3
+#coding=utf-8
+
+'''
+进行猫眼电影排行Top100抓取
+'''
+import json
+import re
+from multiprocessing.pool import Pool
+import requests
+from requests.exceptions import RequestException
+
+#获取一页的内容；
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException as e:
+        return e
+
+#利用正则抓取想要的信息；
+def parse_one_page(html):
+    print("=======================")
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         +'.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                          +'.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>''',re.S)
+    items = re.findall(pattern,html)
+    for item in items:
+        yield{
+            "index":item[0],
+            "image":item[1],
+            "title":item[2],
+            "actor":item[3].strip()[3:],
+            "time":item[4].strip()[5:],
+            "score":item[5]+item[6]
+        }
+
+#将获取的内容逐条写进文件中；
+def write_to_file(content):
+    with open('result.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii = False)+'\n')
+        f.close()
+
+def main(offset):
+    url = "http://maoyan.com/board/4?offset="+str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+if __name__ == "__main__":
+    # for i in range(0,100,10):
+    #     main(i)
+    # 多进程替代单进程的爬取内容，不足之处便是顺序的问题；
+    pool = Pool()
+    pool.map(main,[i for i in range(0,100,10)])
--- demo/test.py
+++ demo/test.py
@@ -14,7 +14,14 @@ from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
+headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.4882.400 QQBrowser/9.7.13059.400',
+        'referer':'http://www.taobao.com'
+    }
 
+chrome_options.add_argument('Accept="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"')
 browser = webdriver.Chrome(r'D:\soft\chromedriver_win32\chromedriver.exe')
 wait = WebDriverWait(browser,10)
 data = "H4sIAAAAAAAAAC2Qy3LCMAxF/0XrhLH8kpxdgNAWCn1AH3STSR34Abrr9N97Yxh7ro6la1v2L7XU0Hy97+ebTW8NJ6OcbmAdVdQqDBKz5iRRWL13rIPnMYWT6pmt99HAN4dtu1I22nc9QMItxmtkI303+SyMi91xQkcNGwPw1CAsULlc8pB/LqguJ1+7BHXUBGtnHCpaFeNd0Xtq0GeY0g9ArzFoRWtg4Ki2og01CcVHZKKvaHtd7m7Lp3LIc9EX3LXvy8B9r+UpgH0pHtAllJE9ZzmNacy1qJxrL+67HrJobazlPIpxo1XsO+CN+MA0c8F7kYiMK0e9FX0v+lH0E91UdCz8hV0hsmHBtMb9/QNRAxRWngEAAA=="
@@ -35,5 +42,5 @@ finally:
 
 
     time.sleep(5)
-    browser.close()
+    #browser.close()
 

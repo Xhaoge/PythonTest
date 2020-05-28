6b8a4c8163284c944fe4892448ac847ec1436359
xiaohaogge <1026737316@qq.com>
2020-05-28 17:25:26 +0800

    add pythontest

---
+++ pythonTest/therdingorder.py
@@ -0,0 +1,21 @@
+search_url = "test-api.gloryholiday.com/standard/search"
+verify_url = "test-api.gloryholiday.com/standard/verify"
+order_url = "test-api.gloryholiday.com/standard/order"
+ticketing_url = "test-api.gloryholiday.com/standard/ticketing"
+
+class Iwofly():
+    def search(self):
+        pass
+    
+    def verify(self,routing_dict):
+        pass
+    
+    def order(self,routing_dict):
+        pass
+
+    def ticketing(self,yuetupnr):
+        pass
+
+    def run(slef):
+        resrouting = self.search()
+        print(resrouting)
